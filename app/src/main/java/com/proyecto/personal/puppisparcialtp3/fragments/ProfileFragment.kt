a957877aa6fb54fbb0fec640d7a526e8b367adcc
Camila <camilaszesko2001@gmail.com>
2023-11-05 18:39:26 -0300

    profile funciona falta alinear y boton de volver atras

--- app/src/main/java/com/proyecto/personal/puppisparcialtp3/fragments/ProfileFragment.kt
+++ app/src/main/java/com/proyecto/personal/puppisparcialtp3/fragments/ProfileFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
 import androidx.fragment.app.Fragment
 import com.proyecto.personal.puppisparcialtp3.R
@@ -14,6 +16,8 @@ import com.bumptech.glide.Glide
 class ProfileFragment : Fragment(){
     lateinit var vista: View
     lateinit var image: ImageView
+    lateinit var buttonUpload: Button
+    lateinit var editTextUrl: EditText
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -26,7 +30,32 @@ class ProfileFragment : Fragment(){
             .load("https://images.dog.ceo/breeds/terrier-toy/n02087046_7037.jpg")
             .into(image)
 
+        editTextUrl = vista.findViewById(R.id.editTextUploadURLProfile)
+        buttonUpload = vista.findViewById(R.id.btnProfileUpload)
+        buttonUpload.setOnClickListener {
+            if (editTextUrl.visibility == View.VISIBLE) {
+                editTextUrl.visibility = View.GONE
+            } else {
+                editTextUrl.visibility = View.VISIBLE
+            }
+
+            val imageUrl = editTextUrl.text.toString()
+            Glide.with(this).clear(image)
+            // Check if the URL is not empty
+            if (imageUrl.isNotEmpty()) {
+                // Use Glide to load the image into the ImageView
+
+                Glide.with(this)
+                    .load(imageUrl)
+                    .into(image)
+            } else {
+                Glide.with(this)
+                    .load(R.drawable.ic_error_loading)
+                    .into(image)
+            }
+        }
         return vista
+
     }
 
 
