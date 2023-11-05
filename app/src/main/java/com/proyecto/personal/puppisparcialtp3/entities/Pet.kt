8c4b614d7b9bd59757764436fc8dbcc7c82833e3
Camila Ingberg <camilaingberg@gmail.com>
2023-11-05 13:11:50 -0300

    rooms added

--- app/src/main/java/com/proyecto/personal/puppisparcialtp3/entities/Pet.kt
+++ app/src/main/java/com/proyecto/personal/puppisparcialtp3/entities/Pet.kt
@@ -15,47 +15,57 @@ class Pet(
     weight: Double,
     location: Location,
     ownerName: String,
-    urlImages: MutableList<String>,
     isAdopted: Boolean,
     isFavorite: Boolean
 ) {
     @PrimaryKey(autoGenerate = true)
     @ColumnInfo(name = "id")
-    val id: Int = 0
+    var id: Int = 0
 
     @ColumnInfo(name = "name")
-    var name: String = name
+    var name: String
 
     @ColumnInfo(name = "age")
-    var age: Int = age
+    var age: Int
 
     @ColumnInfo(name = "breed")
-    var breed: String = breed
+    var breed: String
 
     @ColumnInfo(name = "subBreed")
-    var subBreed: String? = subBreed
+    var subBreed: String?
 
     @ColumnInfo(name = "gender")
-    var gender: Gender = gender
+    var gender: Gender
 
     @ColumnInfo(name = "description")
-    var description: String? = description
+    var description: String?
 
     @ColumnInfo(name = "weight")
-    var weight: Double = weight
+    var weight: Double
 
     @ColumnInfo(name = "location")
-    var location: Location = location
+    var location: Location
 
     @ColumnInfo(name = "ownerName")
-    var ownerName: String = ownerName
-
-    @ColumnInfo(name = "urlImages")
-    var urlImages: MutableList<String> = urlImages
+    var ownerName: String
 
     @ColumnInfo(name = "isAdopted")
-    var isAdopted: Boolean = isAdopted
+    var isAdopted: Boolean
 
     @ColumnInfo(name = "isFavorite")
-    var isFavorite: Boolean = isFavorite
+    var isFavorite: Boolean
+
+    init {
+        this.name = name
+        this.age = age
+        this.breed = breed
+        this.subBreed = subBreed
+        this.gender = gender
+        this.description = description
+        this.weight = weight
+        this.location = location
+        this.ownerName = ownerName
+        this.isAdopted = isAdopted
+        this.isFavorite = isFavorite
+    }
 }
