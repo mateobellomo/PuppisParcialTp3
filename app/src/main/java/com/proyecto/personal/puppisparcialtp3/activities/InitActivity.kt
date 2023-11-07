ee645363e0c7beb8d7bcef33d3a25d5bff9f03c9
Camila Ingberg <camilaingberg@gmail.com>
2023-11-06 22:14:49 -0300

    persistance added

--- app/src/main/java/com/proyecto/personal/puppisparcialtp3/activities/InitActivity.kt
+++ app/src/main/java/com/proyecto/personal/puppisparcialtp3/activities/InitActivity.kt
@@ -1,7 +1,10 @@
 package com.proyecto.personal.puppisparcialtp3.activities
 
+import android.content.ContentValues
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.proyecto.personal.puppisparcialtp3.R
 import com.proyecto.personal.puppisparcialtp3.helpers.SharedPref
 
@@ -10,5 +13,12 @@ class InitActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_init)
         SharedPref.init(applicationContext);
+
+        val userName = SharedPref.read(SharedPref.NAME, null)
+        if (userName != null) {
+            // User is signed in
+            val i = Intent(this@InitActivity, HomeActivity::class.java)
+            startActivity(i)
+        }
     }
 }
