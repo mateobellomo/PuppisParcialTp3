d2d6cbdff9005898eb6cd9d2e610ec2cee65f35a
Camila Ingberg <camilaingberg@gmail.com>
2023-11-07 00:53:26 -0300

    name added to profile

--- app/src/main/java/com/proyecto/personal/puppisparcialtp3/fragments/ProfileFragment.kt
+++ app/src/main/java/com/proyecto/personal/puppisparcialtp3/fragments/ProfileFragment.kt
@@ -7,31 +7,36 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.proyecto.personal.puppisparcialtp3.R
 import com.bumptech.glide.Glide
-
+import com.proyecto.personal.puppisparcialtp3.helpers.SharedPref
 
 
 class ProfileFragment : Fragment(){
-    lateinit var vista: View
-    lateinit var image: ImageView
-    lateinit var buttonUpload: Button
-    lateinit var editTextUrl: EditText
+    private lateinit var view: View
+    private lateinit var image: ImageView
+    private lateinit var buttonUpload: Button
+    private lateinit var editTextUrl: EditText
+    private lateinit var nameTextView: TextView
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        vista = inflater.inflate(R.layout.fragment_profile, container, false)
-        image = vista.findViewById<ImageView>(R.id.imgViewProfile)
-        Glide.with(vista)
+        view = inflater.inflate(R.layout.fragment_profile, container, false)
+        image = view.findViewById<ImageView>(R.id.imgViewProfile)
+        Glide.with(view)
             .load("https://images.dog.ceo/breeds/terrier-toy/n02087046_7037.jpg")
             .into(image)
 
-        editTextUrl = vista.findViewById(R.id.editTextUploadURLProfile)
-        buttonUpload = vista.findViewById(R.id.btnProfileUpload)
+        editTextUrl = view.findViewById(R.id.editTextUploadURLProfile)
+        buttonUpload = view.findViewById(R.id.btnProfileUpload)
+        nameTextView = view.findViewById(R.id.textView)
+        val name = SharedPref.read(SharedPref.NAME, null)
+        nameTextView.text = name
         buttonUpload.setOnClickListener {
             if (editTextUrl.visibility == View.VISIBLE) {
                 editTextUrl.visibility = View.GONE
@@ -44,7 +49,6 @@ class ProfileFragment : Fragment(){
             // Check if the URL is not empty
             if (imageUrl.isNotEmpty()) {
                 // Use Glide to load the image into the ImageView
-
                 Glide.with(this)
                     .load(imageUrl)
                     .into(image)
@@ -54,8 +58,7 @@ class ProfileFragment : Fragment(){
                     .into(image)
             }
         }
-        return vista
-
+        return view
     }
 
 
